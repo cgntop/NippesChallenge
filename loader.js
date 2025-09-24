0fb15570aa8e976bd2f0c9db7b7e626440f9ca20
cgntop <tobklein@gmail.com>
2025-09-24 21:31:48 +0200

    uploag chaddy input

---
+++ loader.js
@@ -0,0 +1,6 @@
+
+async function loadConfig(){
+  const res = await fetch('config.json?' + Date.now());
+  if(!res.ok){ throw new Error('config.json nicht gefunden'); }
+  return await res.json();
+}
